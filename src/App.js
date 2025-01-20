6264fd05e717125a22440df294503a55fcef6418
ifouzezi <ifoghaleuzezi@gmail.com>
2025-01-20 19:31:24 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './index.css';
 import './App.css';
-import { Routes, Route, HashRouter as Router } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/navbar';
 import {
   Home,
@@ -13,14 +13,11 @@ function App() {
   return (
     <div className='min-h-screen p-6 bg-gray-100 text-gray-600 text-lg'>
       <Navbar />
-<Router>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/favorites' element={<Favorites />} />
         <Route path='/recipe-item/:id' element={<Details />} />
       </Routes>
-
-</Router>
     </div>
   );
 }
